e07180569a61b97d8abedb9f3a6e55083af63964
Ryan Erickson <ryan.erickson@lairdconnect.com>
2022-03-10 14:28:19 -0600

    Enable firmware update over UART
    Enable mcumgr and firmware update over UART.
    
    Generate build ID for firmware and set it in app  version string.
    
    bug 21177

--- src/main.c
+++ src/main.c
@@ -9,9 +9,16 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 
 #include <zephyr.h>
 
+#ifdef CONFIG_LCZ_MCUMGR_WRAPPER
+#include "mcumgr_wrapper.h"
+#endif
+
 #include "app_version.h"
 
 void main(void)
 {
 	LOG_INF("BLE Gateway DM Firmware v%s [%s]", APP_VERSION_STRING, CONFIG_BOARD);
+#ifdef CONFIG_LCZ_MCUMGR_WRAPPER
+	mcumgr_wrapper_register_subsystems();
+#endif
 }
