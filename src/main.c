8bdb9f0ceb8caf8dfb3f1158c83c6714bdf56a93
Ryan Erickson <ryan.erickson@lairdconnect.com>
2022-03-22 09:25:21 -0500

    Turn on support for file system (LFS)
    Enable file system and mount LFS partition.
    Enable file system shell for easy testing.
    
    bug 21177

--- src/main.c
+++ src/main.c
@@ -12,12 +12,20 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #ifdef CONFIG_LCZ_MCUMGR_WRAPPER
 #include "mcumgr_wrapper.h"
 #endif
+#ifdef CONFIG_FILE_SYSTEM_UTILITIES
+#include "file_system_utilities.h"
+#endif
 
 #include "app_version.h"
 
 void main(void)
 {
 	LOG_INF("BLE Gateway DM Firmware v%s [%s]", APP_VERSION_STRING, CONFIG_BOARD);
+
+#ifdef CONFIG_FILE_SYSTEM_UTILITIES
+	fsu_lfs_mount();
+#endif
+
 #ifdef CONFIG_LCZ_MCUMGR_WRAPPER
 	mcumgr_wrapper_register_subsystems();
 #endif
