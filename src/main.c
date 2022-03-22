726a4eced5231fc7927a9103121f677e0f57f678
Ryan Erickson <ryan.erickson@lairdconnect.com>
2022-03-22 09:34:21 -0500

    Enable hardware unique key (HUK)
    Use the HUK API to allow the device to generate
    a HUK if one does not already exist. The HUK
    will allow the device to encrypt data that only it can decrypt.
    
    Turn off CONFIG_DISABLE_FLASH_PATCH for now
    because it doesn't work with CONFIG_HW_UNIQUE_KEY_LOAD.
    
    Add automated tests to allow easy way to build
    for all platforms this firmware supports.
    
    bug 21177

--- src/main.c
+++ src/main.c
@@ -15,12 +15,17 @@ LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
 #ifdef CONFIG_FILE_SYSTEM_UTILITIES
 #include "file_system_utilities.h"
 #endif
+#ifdef CONFIG_LCZ_HW_KEY
+#include "lcz_hw_key.h"
+#endif
 
 #include "app_version.h"
 
 void main(void)
 {
-	LOG_INF("BLE Gateway DM Firmware v%s [%s]", APP_VERSION_STRING, CONFIG_BOARD);
+#ifdef CONFIG_LCZ_HW_KEY
+	lcz_hw_key_generate_and_init();
+#endif
 
 #ifdef CONFIG_FILE_SYSTEM_UTILITIES
 	fsu_lfs_mount();
@@ -29,4 +34,6 @@ void main(void)
 #ifdef CONFIG_LCZ_MCUMGR_WRAPPER
 	mcumgr_wrapper_register_subsystems();
 #endif
+
+	LOG_INF("BLE Gateway DM Firmware v%s [%s]", APP_VERSION_STRING, CONFIG_BOARD);
 }
